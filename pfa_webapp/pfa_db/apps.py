c7dd82762cd0ea0dd7cd0f3ecb01d492fc89f4e3
AmineDT <amine.elbakkouri@outlook.com>
2022-05-16 23:29:04 +0100

    Base de données initialisation

---
+++ pfa_webapp/pfa_db/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PfaDbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pfa_db'
---
+++ pfa_webapp/pfa_db/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Students(models.Model):
+    id_student = models.AutoField(primary_key=True)
+    name_student = models.CharField(max_length=-1)
+    surname_student = models.CharField(max_length=-1)
+    study_field = models.CharField(max_length=-1, blank=True, null=True)
+    email = models.CharField(max_length=-1, blank=True, null=True)
+    phone = models.CharField(max_length=-1, blank=True, null=True)
+
+
+class Label(models.Model):
+    id_label = models.AutoField(primary_key=True)
+    name_label = models.CharField(max_length=-1)
+    description = models.TextField(blank=True, null=True)
+
+
+class Events(models.Model):
+    id_event = models.AutoField(primary_key=True)
+    name_event = models.CharField(max_length=-1)
+    id_label = models.ForeignKey('Label', models.DO_NOTHING, db_column='id_label', blank=True, null=True)
+    date_event = models.DateField(blank=True, null=True)
+    duration = models.DurationField(blank=True, null=True)
+    budget = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    available_places = models.IntegerField(blank=True, null=True)
+
+
+class Treasury(models.Model):
+    id_purchase = models.AutoField(primary_key=True)
+    article = models.CharField(max_length=-1, blank=True, null=True)
+    unitary_cost = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    purchase_date = models.DateField(blank=True, null=True)
+    invoice_number = models.CharField(max_length=-1, blank=True, null=True)
+
+
+class Tickets(models.Model):
+    id_event = models.OneToOneField(Events, models.DO_NOTHING, db_column='id_event', primary_key=True)
+    id_student = models.ForeignKey(Students, models.DO_NOTHING, db_column='id_student')
+    ticket_price = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    presence = models.BooleanField(blank=True, null=True)
+
